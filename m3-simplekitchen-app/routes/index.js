c3357f7fcdd281a6391471e7d7da9570c68f66d7
Fan <52841919+fantastic-y@users.noreply.github.com>
2021-10-09 10:15:57 -0700

    copy the kitchen app

---
+++ m3-simplekitchen-app/routes/index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const auth = require('http-auth');
+const { check, validationResult } = require('express-validator');
+
+const router = express.Router();
+const Registration = mongoose.model('Registration');
+const basic = auth.basic({
+  file: path.join(__dirname, '../users.htpasswd'),
+});
+
+router.get('/', (req, res) => {
+  //res.send('It works!');
+  res.render('home', { title: 'Home Page' });
+});
+
+router.get('/register', (req, res) => {
+  //res.send('It works!');
+  res.render('form', { title: 'Registration form' });
+});
+
+router.get('/registrants', basic.check((req, res) => {
+  Registration.find()
+    .then((registrations) => {
+      res.render('registrants', { title: 'Listing registrations', registrations });
+    })
+    .catch(() => { 
+      res.send('Sorry! Something went wrong.'); 
+    });
+}));
+
+router.post('/', 
+    [
+        check('name')
+        .isLength({ min: 1 })
+        .withMessage('!Error:Please enter a name'),
+        check('email')
+        .isLength({ min: 1 })
+        .withMessage('!Error:Please enter an email'),
+    ],
+    (req, res) => {
+        //console.log(req.body);
+        const errors = validationResult(req);
+        if (errors.isEmpty()) {
+          const registration = new Registration(req.body);
+          registration.save()
+            .then(() => {res.render('thankyou', { title: 'Thank you for Registration' });})
+            .catch((err) => {
+              console.log(err);
+              res.send('Sorry! Something went wrong.');
+            });
+          } else {
+            res.render('form', { 
+                title: 'Registration form',
+                errors: errors.array(),
+                data: req.body,
+             });
+          }
+    });
+
+module.exports = router;
